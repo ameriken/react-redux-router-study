86e1251457985be2ac40fb716fdd51cbda070357
ameriken <hitohahito@hotmail.co.jp>
2018-10-09 22:47:22 +0900

    [add]JSXでreactElementを作る

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from "react";
 import { render } from 'react-dom';
 
-render(<h2>こんにちは世界</h2>, document.getElementById('root'))
+var reactElement= <h2>こんにちは世界</h2>;
+render(reactElement, document.getElementById('root'))
