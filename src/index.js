428600ee7557789abc819b35573d94b10fc2c6a0
ameriken <hitohahito@hotmail.co.jp>
2018-10-11 02:18:44 +0900

    [add]part1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,39 @@
 import React from "react";
 import { render } from 'react-dom';
 
-render(<h2>こんにちは世界</h2>, document.getElementById('root'))
+
+class H2O extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { temp: 15 };
+    }
+
+    render () {
+        return (
+            <div>
+                <h2>{this.state.temp}</h2>
+                <button onClick={this.onPlusClick}>+</button>
+                <button onClick={this.onPlus10Click}>+10</button>
+                <button onClick={this.onMinusClick}>-</button>
+                <button onClick={this.onMinus10Click}>-10</button>
+            </div>
+        )
+    }
+
+    onPlusClick= () => {
+        this.setState({ temp: this.state.temp + 1});
+    }
+
+    onPlus10Click= () => {
+        this.setState({ temp: this.state.temp + 10});
+    }
+
+    onMinusClick= () => {
+        this.setState({ temp: this.state.temp - 1});
+    }
+
+    onMinus10Click= () => {
+        this.setState({ temp: this.state.temp - 10});
+    }
+}
+render(<H2O />, document.getElementById('root'))
