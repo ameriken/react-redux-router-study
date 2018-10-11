008f603f9f0fbaa329afb3c9272de69919ae8f02
ameriken <hitohahito@hotmail.co.jp>
2018-10-11 23:27:34 +0900

    [add]filterのサンプル

--- src/index.js
+++ src/index.js
@@ -2,15 +2,21 @@ const array1 = [0, 1, 2, 3, 4];
 
 // indexは配列の何番目か
 // ouputは配列の中身
-const newArray1 = array1.map((output,index) => {
-    return `${index}番目は${output}です。`;
+// trueが返す場合のみ配列だけを採用
+const newArray1 = array1.filter((output,index) => {
+    return output > 3
 });
 
 console.log(newArray1);
 
-//この例がわかりやすくて、phpでいうforeach();
 const array2 = ["tanaka", "nakanishi", "sasaki", "koyama", "oda"];
-const newArray2 = array2.map((output,index) => {
-    return `${index}番目は${output}です。`;
+const newArray2 = array2.filter((output,index) => {
+    return output === 'nakanishi';
 });
 console.log(newArray2);
+
+const array3 = ["nakanishi", "Furukawa", "tanaka"];
+const newArray3 = array3.filter((output,index) => {
+    return output.length > 7;
+});
+console.log(newArray3);
