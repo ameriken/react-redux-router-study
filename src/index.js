5cdfc1be6190499e5a61e58a46b1bdac283c753a
ameriken <hitohahito@hotmail.co.jp>
2018-10-11 23:22:43 +0900

    [add]mapサンプル

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
-import React from "react";
-import { render } from 'react-dom';
+const array1 = [0, 1, 2, 3, 4];
 
-render(<h2>こんにちは世界</h2>, document.getElementById('root'))
+// indexは配列の何番目か
+// ouputは配列の中身
+const newArray1 = array1.map((output,index) => {
+    return `${index}番目は${output}です。`;
+});
+
+console.log(newArray1);
+
+//この例がわかりやすくて、phpでいうforeach();
+const array2 = ["tanaka", "nakanishi", "sasaki", "koyama", "oda"];
+const newArray2 = array2.map((output,index) => {
+    return `${index}番目は${output}です。`;
+});
+console.log(newArray2);
