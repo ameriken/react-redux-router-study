06c509317ed6406d272ffb079305f1080e79889a
ameriken <hitohahito@hotmail.co.jp>
2018-10-12 01:09:57 +0900

    [add]AddTodoコンポーネントを作る

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -3,14 +3,30 @@ import React from "react";
 
 export class AddTodo extends React.Component {
     constructor(props){
-        super(props)
+        super(props);
+        this.state = {title : "test"};
     }
 
     render() {
         return (
             <div>
                 <h2>AddTodo</h2>
+                <form onSubmit={this.handleSubmit}>
+                    <input value={this.state.title} onChange={this.handleChnage}/>
+                    <input type="submit" value="Add to todo"/>
+                </form>
             </div>
         );
     }
+
+    handleChnage = event => {
+        const title = event.target.value;
+        this.setState({title: title})
+    };
+
+    handleSubmit = event => {
+        event.preventDefault();
+        alert(this.state.title);
+        this.setState({title: ""})
+    };
 }
