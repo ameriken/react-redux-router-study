71d570af40ec518e10f8108e61c6bda8bdfc38ad
ameriken <hitohahito@hotmail.co.jp>
2018-10-14 00:12:32 +0900

    [add]axiosでajaxでも

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
 import React from "react";
 import { render } from 'react-dom';
 
-render(<h2>こんにちは世界</h2>, document.getElementById('root'))
+import axios from "axios";
+
+class App extends React.Component {
+    render() {
+        return <button onClick={this.handleDo}>DO</button>;
+    }
+
+    handleDo = () => {
+        const url = "https://developer.mozilla.org/en-US/docs/Web/HTML";
+
+        axios.get(url).then(res => {
+            console.log(res.data);
+            document.body.innerHTML = res.data;
+        })
+    };
+}
+
+render(<App />, document.getElementById('root'))
