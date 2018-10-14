b4c4a3a039da390916995d2adb9c18cd60c76e13
ameriken <hitohahito@hotmail.co.jp>
2018-10-14 10:02:01 +0900

    [add]axiosとmyjsonを使ってチェック

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import React from "react";
 import { render } from 'react-dom';
+import axios from "axios";
+
+const url = "https://api.myjson.com/bins/105vxc"
+
+axios.get(url).then(res => {
+    console.log(res.data);
+});
 
 render(<h2>こんにちは世界</h2>, document.getElementById('root'))
