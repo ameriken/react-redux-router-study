6f6d9360929e8d97e715205e2a1fe2766a9145dc
ameriken <hitohahito@hotmail.co.jp>
2018-10-14 10:21:32 +0900

    [add]axiosとjsonの連携テスト

--- src/index.js
+++ src/index.js
@@ -1,11 +1,41 @@
 import React from "react";
 import { render } from 'react-dom';
+
 import axios from "axios";
 
-const url = "https://api.myjson.com/bins/105vxc"
+class App extends React.Component {
 
-axios.get(url).then(res => {
-    console.log(res.data);
-});
+    constructor() {
+        super();
+        this.state = { member: [] };
+    }
 
-render(<h2>こんにちは世界</h2>, document.getElementById('root'))
+    memberList(list) {
+        const memberList = list.map((member, index) => {
+            return (
+                <li>{member.name} {member.age} </li>
+            );
+        });
+        return <ul>{memberList}</ul>;
+    }
+
+    render() {
+        console.log(this.state.member);
+        return (
+            <div>
+                <button onClick={this.getJson}>Get Json</button>
+                {this.memberList(this.state.member)}
+            </div>
+        );
+    }
+
+    getJson = () => {
+        const url = "https://api.myjson.com/bins/159wqn";
+        axios.get(url).then(res => {
+            this.setState(res.data)
+        });
+    };
+}
+
+
+render(<App />, document.getElementById('root'));
