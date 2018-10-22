5e7262efd3abfeee8e1685e15be66769d98c0ce1
ameriken <hitohahito@hotmail.co.jp>
2018-10-23 00:59:26 +0900

    [add]コンポーネントに配列渡す

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from "react";
 import { render } from 'react-dom';
-import MyType from './MyType';
+import MyAttrMulti from './MyAttrMulti';
+
+const data = {
+    name: 'あめりけん権左衛門',
+    age: 18,
+    sex: '男',
+};
 
 render(
-    <div>
-        <MyType value="あめりけん" />
-        <MyType value={"あめりけん"} />
-        <MyType value ={108} />
-        <MyType value ={true} />
-        <MyType value ={['うめ','もも','さくら']} />
-        <MyType value ={{name: "山田進太郎",age:40}} />
-        <MyType value ={() => console.log('Hoge')} />
-    </div>
-    , document.getElementById('root'))
+    <MyAttrMulti{...data} />,
+    document.getElementById('root'))
