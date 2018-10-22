88c7661dcf5d2d1aec43b82e35120d640e117e3b
ameriken <hitohahito@hotmail.co.jp>
2018-10-23 01:47:06 +0900

    [add]propstypes動かないけど、やり方あってるはず

--- src/MyHello.js
+++ src/MyHello.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class MyHello extends Component {
     render() {
         return <div>こんんちは{this.props.name}さん！</div>;
     }
-
 }
 
+MyHello.propTypes = {
+    name: PropTypes.string.isRequired
+};
--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
 import React from "react";
 import { render } from 'react-dom';
-import MyBook from "./MyBook";
-
-const book = {
-    isbn: 'WGS-JST-001',
-    title: '速習 webpack',
-    price: 454,
-    published: 'WINGSプロジェクト'
-};
+import MyHello from './MyHello'
 
 render(
-    <MyBook info={book} />,
+    <MyHello name={'ameriken'} />,
     document.getElementById('root')
 );
